928c89acf7cc808dc69cef5783bc72be53f35c28
aldrinmcaalim <aldrinmcaalim@gmail.com>
2023-03-02 16:56:43 -0500

    added List component and delete logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+import peopleData from "./data";
+
+import List from "./components/list/List";
+
 function App() {
+  const [people, setPeople] = useState(peopleData);
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
